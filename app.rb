7d69f2a2de15ecb12e063ef9950d2f21036f4e04
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-06-02 15:29:38 -0400

    started Get and Post requests

--- app.rb
+++ app.rb
@@ -1,48 +1,49 @@
 require 'sinatra/base'
+require 'HTTParty'
 require 'sinatra/json'
 require 'json'
-some change
+require './Event.rb'
+
 class EventPlannerApp < Sinatra::Base
-  set :logging, true
-  set :show_exceptions, false
-  error do |e|
-    binding.pry
-    raise e
-  end
-
-  DB = {}
-
-  before do
-    require_authorization!
-  end
-
-  def require_authorization!
-    unless username
-      status 401
-      halt({ error: "You must log in" }.to_json)
+    set :logging, true
+    set :show_exceptions, false
+    error do |e|
+        binding.pry
+        raise e
     end
-  end
 
-  def username
-    request.env["HTTP_AUTHORIZATION"]
-  end
-end
+    DB = {}
 
-   get "/calendar" do
-    DB[username] ||= []
-    json DB[username]
+    before do
+        require_authorization!
+    end
+
+    def require_authorization!
+        unless username
+            status 401
+            halt({ error: "You must log in" }.to_json)
+        end
+    end
+
+    def username
+        request.env["HTTP_AUTHORIZATION"]
+    end
+
+    get "/calendar" do
+        DB[username] ||= []
+        json DB[username]
     end
 
     post "/calendar" do
         body = request.body.read
 
         begin
-            new_event =Event.new(JSON.parse body)
+            event =Event.new(JSON.parse body)
         rescue
             status 400
             halt "Can't parse json: '#{body}'"
         end
-        if new_event.valid?
+        if event.valid?
             DB[username] ||= []
             DB[username].push new_event
         else
@@ -54,9 +55,9 @@ end
 
 
     end
-
-
+end
 
 if $PROGRAM_NAME == __FILE__
     EventPlannerApp.run!
 end
+binding.pry
