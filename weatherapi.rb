2f46e55ec66cf113e1bf8c5b041f2ae941c5436b
tythompson <tythompson@gmail.com>
2016-06-02 19:19:14 -0400

    Working Weather API calls

--- weatherapi.rb
+++ weatherapi.rb
@@ -1,26 +1,27 @@
-# require 'open-uri'
 require 'json'
 require './token'
 require 'pry'
-require 'httparty'
+require 'HTTParty'
 
-# location = data.json
-# current conditions api example = 'http://api.wunderground.com/api/4fea5ef877495d40/geolookup/conditions/q/IA/Cedar_Rapids.json'
-# path = '.\data.json'
-#local file JSON.parse File.read path
-#api call open('location')
-
-def fetch_weather(zipcode)
-   HTTParty.get("http://api.wunderground.com/api/#{@token}/geolookup/conditions/q/#{zipcode}.json")
+# Date example 06020602
+#              MMDD-MMDD
+def fetch_weather(zipcode,date)
+	return HTTParty.get("http://api.wunderground.com/api/#{@token}/planner_#{date}/q/#{zipcode}.json")
 end
 
-def fetch_weather_test
-  HTTParty.get("http://api.wunderground.com/api/4fea5ef877495d40/geolookup/conditions/q/IA/Cedar_Rapids.json")
+def rain_chance(fetch_data)
+	fetch_data['trip']['chance_of']['chanceofprecip']['percentage']
 end
 
-temp = fetch_weather_test["current_observation"]["temp_f"]
-weather = fetch_weather_test["current_observation"]["weather"]
-tempz = fetch_weather(27511)["current_observation"]["temp_f"]
-weather = fetch_weather(27511)["current_observation"]["weather"]
+def avg_high(fetch_data)
+	fetch_data['trip']['temp_high']['avg']['F']
+end
 
-binding.pry
+def avg_low(fetch_data)
+	fetch_data['trip']['temp_low']['avg']['F']
+end
+
+fetch_data = fetch_weather(27703,06020602)
+puts "Rain Chance: ", rain_chance(fetch_data)
+puts "Avg High: ", avg_high(fetch_data)
+puts "Avg Low: ", avg_low(fetch_data)
