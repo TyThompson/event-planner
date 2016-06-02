1a9dffd8c1221989515b4f7895e668cfba4614de
tythompson <tythompson@gmail.com>
2016-06-02 16:28:34 -0400

    weather api ready to use

--- weatherapi.rb
+++ weatherapi.rb
@@ -6,26 +6,21 @@ require 'httparty'
 
 # location = data.json
 # current conditions api example = 'http://api.wunderground.com/api/4fea5ef877495d40/geolookup/conditions/q/IA/Cedar_Rapids.json'
-path = '.\data.json'
-password = @token
+# path = '.\data.json'
 #local file JSON.parse File.read path
 #api call open('location')
-# JSON.parse File.read 'data.json' do |f|
 
-# def fetch_weather(zipcode)
-#   HTTParty.get("http://api.wunderground.com/api/#{password}/geolookup/conditions/q/#{@zipcode}.json")
-# end
+def fetch_weather(zipcode)
+   HTTParty.get("http://api.wunderground.com/api/#{@token}/geolookup/conditions/q/#{zipcode}.json")
+end
 
 def fetch_weather_test
   HTTParty.get("http://api.wunderground.com/api/4fea5ef877495d40/geolookup/conditions/q/IA/Cedar_Rapids.json")
 end
 
-f = fetch_weather_test
-test = f.values_at('weather','temp_f')
-  # print "fetching weather stats"
-  # location = JSON.parse['location']['city']
-  # temp_f = JSON.parse['current_observation']['temp_f']
-  # current_weather = JSON.parse['weather']['current_weather']
-  # print "Current temperature in #{location} is: #{temp_f}\n"
-  # print "Current weather is #{current_weather} and the chance of precipitation is eh..."
+temp = fetch_weather_test["current_observation"]["temp_f"]
+weather = fetch_weather_test["current_observation"]["weather"]
+tempz = fetch_weather(27511)["current_observation"]["temp_f"]
+weather = fetch_weather(27511)["current_observation"]["weather"]
+
 binding.pry
