2fc1cb8c104a1adb3f8cfd1cf3a825f7099da2a1
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-06-02 15:29:08 -0400

    validation methods started

--- Event.rb
+++ Event.rb
@@ -1,3 +1,7 @@
+require './app.rb'
+
+
+
 class Event 
 
     attr_reader :event
@@ -10,8 +14,28 @@ class Event
     end
     
     def valid? 
-        true
-        
+       if event_valid? && description_valid? && location.valid? && time_valid?
+           true
+        else
+            false
+        end
+    end
+
+    def title_valid?
+        unless @title == @title.to_i.to_s
+        end
+    end
+    
+    def desc_valid?
+        unless @description == @description.to_i.to_s
+            true
+        end
+    end
+
+    def location_valid?
+        unless @location.count != 5 || !@location.is_an_integer?
+            true
+        end
     end
 
 
