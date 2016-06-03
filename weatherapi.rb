76e7843b1f57cdbac0b5fbd343e9bedb8613e6fc
tythompson <tythompson@gmail.com>
2016-06-03 14:32:49 -0400

    added new api dummy data

--- weatherapi.rb
+++ weatherapi.rb
@@ -7,27 +7,19 @@ require 'Time'
 
 
 def fetch_weather(zipcode)
-	print "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=#{@token2}&q=#{zipcode}&tp=24&format=json"
 	HTTParty.get("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=#{@token2}&q=#{zipcode}&tp=24&format=json")
 	# data = JSON.parse File.read '.\plannerdata.json'
-  #date format is yy-mm-dd
 end
 
-# def check_call
-# 	HTTParty.get("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=2528be4ba1064395877155302160306&q=48.85,2.35&num_of_days=1&tp=3&format=json")
-# end
-
 def rain_chance(fetch_data, days)
-  # Time.strftime ("date") - Time.now.strftime("%m-%d")
-  # modifier = (endDate - beginDate).to_i
 	fetch_data['data']['weather'][days]['hourly'][0]['chanceofrain']
 end
 
-def avg_high(fetch_data, days)
+def max_temp(fetch_data, days)
 	fetch_data['data']['weather'][days]['maxtempF']
 end
 
-def avg_low(fetch_data, days)
+def min_temp(fetch_data, days)
 	fetch_data['data']['weather'][days]['mintempF']
 end
 
@@ -37,13 +29,28 @@ def date_math(month, day)
   days = (endDate - beginDate).to_i
 end
 
+def display_weather(month, day, zip)
+  fetch_data = fetch_weather("zip")
+  days = date_math(month, day)
+  if date_math(month, day) < 13
+    print "Avg High: ", max_temp(fetch_data, days)
+    puts
+    print "Avg Low: ", min_temp(fetch_data, days)
+    puts
+    print "Rain Chance: ", rain_chance(fetch_data, days)
+  else
+    print "Can only check weather 2 weeks in advance."
+  end
+end
+display_weather(06,06,27703)
+binding.pry
 
 #15 days or greater don't get weather
 					# Date example 06020602
 					#              MMDD-MMDD
 
-fetch_data = fetch_weather("27703")
-days = date_math(06, 06)
+# fetch_data = fetch_weather("27703")
+# days = date_math(06, 06)
 #               month, day
 # out_of_calls = JSON.parse File.read 'out_of_calls.json'
 # dummy_data = JSON.parse File.read 'plannerdata.json'
@@ -55,6 +62,3 @@ days = date_math(06, 06)
 # 	puts "Using real data"
 # 	fetch_data
 # end
-puts "Rain Chance: ", rain_chance(fetch_data, days)
-puts "Avg High: ", avg_high(fetch_data, days)
-puts "Avg Low: ", avg_low(fetch_data, days)
