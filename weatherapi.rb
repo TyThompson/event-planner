16f7cf3bacf3e07a7673913745813be9cc413d3a
tythompson <tythompson@gmail.com>
2016-06-02 22:07:12 -0400

    out of calls test

--- weatherapi.rb
+++ weatherapi.rb
@@ -3,10 +3,10 @@ require './token'
 require 'pry'
 require 'HTTParty'
 
-# Date example 06020602
-#              MMDD-MMDD
+
 def fetch_weather(zipcode,date)
-	return HTTParty.get("http://api.wunderground.com/api/#{@token}/planner_#{date}/q/#{zipcode}.json")
+	HTTParty.get("http://api.wunderground.com/api/#{@token}/planner_#{date}/q/#{zipcode}.json")
+	# data = JSON.parse File.read '.\plannerdata.json'
 end
 
 def rain_chance(fetch_data)
@@ -21,7 +21,20 @@ def avg_low(fetch_data)
 	fetch_data['trip']['temp_low']['avg']['F']
 end
 
+					# Date example 06020602
+					#              MMDD-MMDD
+
 fetch_data = fetch_weather(27703,06020602)
+out_of_calls = JSON.parse File.read '.\out_of_calls.json'
+dummy_data = JSON.parse File.read '.\plannerdata.json'
+if fetch_data.to_json == out_of_calls.to_json
+	fetch_data = dummy_data
+	puts "Using dummy data, we are out of calls"
+else
+	puts "Using real data"
+end
+
 puts "Rain Chance: ", rain_chance(fetch_data)
 puts "Avg High: ", avg_high(fetch_data)
 puts "Avg Low: ", avg_low(fetch_data)
+
