29347586ed103bbfb55ad4bcfec6b9dee5f5b0be
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-06-05 16:39:26 -0400

    added input validation checks to Event.rb

--- Event.rb
+++ Event.rb
@@ -3,38 +3,45 @@ require './app.rb'
 
 class Event
 
-    attr_reader :title, :date, :zipcode, :time
+	attr_reader :title, :date, :zipcode, :time
 
-    def initialize event
-        @title = event["title"]
-        @date = event["date"]
-        @zipcode = event["zipcode"]
-        @time = event["time"]
+	def initialize event
+		@title = event["title"]
+		@date = event["date"]
+		@zipcode = event["zipcode"]
+		@time = event["time"]
 
 
-    end
+	end
 
-    def valid?
-        #  if event_valid? && description_valid? && location.valid? && time_valid?
-        true
-        #   else
-        #       false
-        #   end
-    end
+	def valid?
+		#  if event_valid? && description_valid? && location.valid? && time_valid?
+		if title_valid? && zipcode_valid? && date_valid?
+			true
+			#   else
+			#       false
+			#   end
+		end
+	end
 
-    def title_valid?
-        unless @title == @title.to_i.to_s
-        end
-    end
+	def title_valid?
+		unless @title == @title.to_i.to_s
+			true
+		end
+	end
 
-    def zipcode_valid?
-        unless @location.count != 5 || !@location.is_an_integer?
-            true
-        end
-    end
+	def zipcode_valid?
+		unless @zipcode.length != 5 || !(@zipcode !~ /\D/) 
+			true
+		end
+	end
 
-    # def fetch_weather zipcode, date
-    #
-    # end
+	def date_valid?
+		unless @date.length != 4 || !(@date !~ /\D/) 
+			true
+		end
+
+	end
+
 
 end
